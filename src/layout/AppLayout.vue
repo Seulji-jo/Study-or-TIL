48b6a9746474bc4d128f94380515a36a6138378b
Seulji-jo <joseuling@gmail.com>
2021-12-13 17:03:47 +0900

    [add] add main Nav layout

---
+++ src/layout/AppLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <header>
+      <div>HOME</div>
+      <nav @mouseover="mouseoverNav">
+        <router-link v-for="(nav, i) of setNav" :key="i" :to="nav.path">
+          {{ nav.meta.title }}
+        </router-link>
+      </nav>
+    </header>
+    <main>
+      <router-view :key="key" />
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isHover: false,
+    };
+  },
+  computed: {
+    setNav() {
+      return this.$router.options.routes.filter(route => route.meta?.title);
+    },
+    key() {
+      return this.$route.path;
+    },
+  },
+  created() {},
+  methods: {
+    mouseoverNav(e) {
+      console.log(e.target);
+    },
+  },
+};
+</script>
+
+<style></style>
